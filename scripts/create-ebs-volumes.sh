79dc760e1501456d0c9dca7b6e3a58e348c0ec5f
Michael G Colletti <michael@cpossibilities.com>
2020-07-22 18:49:45 -0400

    mod scripts for ebs add more pv and pvc definitions

--- scripts/create-ebs-volumes.sh
+++ scripts/create-ebs-volumes.sh
@@ -4,10 +4,13 @@
 
 az=us-east-1a 
 voltypeDB=nitro
-voltypeMON=standard
+voltypeMON=gp2
+voltypeLOG=standard
 sizeDB=40
 sizeMON=50
+sizeLOG=50
 
 aws ec2 create-volume --availability-zone $az --size $sizeDB --volume-type $voltypeDB 
 aws ec2 create-volume --availability-zone $az --size $sizeMON --volume-type $voltypeMON
+aws ec2 create-volume --availability-zone $az --size $sizeLOG --volume-type $voltypeLOG
 #--multi-attach-enabled 
